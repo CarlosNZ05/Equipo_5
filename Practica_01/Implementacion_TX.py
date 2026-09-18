10a50bd4636619761df6968fbc338d8056adc93d
DIEGO SANCHEZ <diego.sanchezr@iteso.mx>
2026-09-18 16:24:18 -0600

    correccion codigo TX

--- Practica_01/Implementacion_TX.py
+++ Practica_01/Implementacion_TX.py
@@ -1,37 +1,45 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-TX_PIN = "P8_10"        
-BAUDRATE = 9600         
-BIT_TIME = 1.0 / BAUDRATE 
+TX_PIN = "P8_10"
+BAUDRATE = 9600
+BIT_TIME = 1.0 / BAUDRATE
+
 
 def setup_uart():
     GPIO.setup(TX_PIN, GPIO.OUT)
+
+    # UART en reposo = HIGH
     GPIO.output(TX_PIN, GPIO.HIGH)
-    time.sleep(0.1) 
+
+    time.sleep(0.1)
+
 
 def uart_tx(byte):
-    #Bit de start
+    # Bit de START
     GPIO.output(TX_PIN, GPIO.LOW)
     time.sleep(BIT_TIME)
 
-    #Bits de datos
+    # 8 bits de datos, LSB primero
     for i in range(8):
-        bit = (byte >> i) & 1 
-        
+        bit = (byte >> i) & 1
+
         if bit == 1:
             GPIO.output(TX_PIN, GPIO.HIGH)
         else:
             GPIO.output(TX_PIN, GPIO.LOW)
-            
+
         time.sleep(BIT_TIME)
 
-    #bit de stop
+    # Bit de STOP
     GPIO.output(TX_PIN, GPIO.HIGH)
     time.sleep(BIT_TIME)
 
 
-    setup_uart()
-    uart_tx(ord('H'))
+
+setup_uart()
+
+while True:
+    uart_tx(ord('I'))
     time.sleep(0.05)
-    uart_tx(ord('i'))
+
